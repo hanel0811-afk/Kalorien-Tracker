a962a49a72ff64240c3d8dbec6f1abdc0aac077e
hanel0811-afk <hanel0811@gmail.com>
2026-04-20 22:19:28 +0200

    Add firebase config for deployment

---
+++ firebase-config.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyD0eo_ebMlfC9SDc9j8-DKwrzF04uQy9ho",
+
+    authDomain: "kalorien-tracker-2421c.firebaseapp.com",
+
+    projectId: "kalorien-tracker-2421c",
+
+    storageBucket: "kalorien-tracker-2421c.firebasestorage.app",
+
+    messagingSenderId: "344939670745",
+
+    appId: "1:344939670745:web:84f98ef642a21e5a8e22c8"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
